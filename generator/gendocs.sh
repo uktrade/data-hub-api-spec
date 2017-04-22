ce4866fa9b8f1f47d45a4c1cf1b08fcb44582562
jimsmith <jim.smith@digital.trade.gov.uk>
2017-04-22 16:48:05 +0100

    built new docs

--- generator/gendocs.sh
+++ generator/gendocs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 java -jar ./lib/swagger-codegen-cli-2.2.2.jar generate -l nodejs-server -i datahub.yml -t src/nodejs-templates/ -o ../gendocs
-mkdir ../gendocs/public
+mkdir -p ../gendocs/public
 node markdown.js
 cp src/public/style.css ../public/
-cp ../datahub.yml ../public/
+cp datahub.yml ../public/
