331e60e13475682b0be85df2d4f9b5e7a0886e4b
jimsmith <jim.smith@digital.trade.gov.uk>
2017-04-25 17:50:00 +0100

    WIP investment project list

--- gendocs/controllers/InvestmentService.js
+++ gendocs/controllers/InvestmentService.js
@@ -196,6 +196,95 @@ exports.getinvestmentvalue = function(args, res, next) {
   }
 }
 
+exports.getprojectsforcompany = function(args, res, next) {
+  /**
+   * returns a summary of investment projects for a company
+   * Returns a summary list of investment projects per companyid - @TODO there is an issue with companyId here - I need to get clarity on which (or any?) of the three company IDs this means
+   *
+   * companyId UUID UUID of a company with investment projects
+   * limit Integer pagination parameter
+   * offset Integer pagination parameter (optional)
+   * returns InvestmentProjectsList
+   **/
+  var examples = {};
+  examples['application/json'] = {
+  "next" : "http://{HOST}/v3/investment/{companyId}/projects?limit=10&offset=5",
+  "previous" : "http://{HOST}/v3/investment/{companyId}/projects?limit=10&offset=5",
+  "count" : 30,
+  "results" : [ {
+    "estimated_land_date" : "2017-05-01",
+    "description" : "Marriott hotels wishes to open in a new part of Manchester under-served by its existing hotels",
+    "referral_source_activity_event" : {
+      "name" : "Event Name",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "fdi_type" : {
+      "name" : "Merger",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "not_shareable_reason" : "Company has asked us to keep project quiet until official announcement",
+    "anonymous_description" : "A large hotel chain wishes to open a hotel in a new part of a large northern city under-served by its existing hotels",
+    "referral_source_activity_marketing" : {
+      "name" : "Press advertisement",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "client_contacts" : "",
+    "client_relationship_manager" : {
+      "last_name" : "Advisor",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+      "first_name" : "Alison"
+    },
+    "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    "sector" : {
+      "name" : "Advanced Engineering",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "document_link" : "https://sp.cdms.ukti.gov.uk/optevia_project/P-30027598",
+    "phase" : {
+      "name" : "Active",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "referral_source_activity_website" : {
+      "name" : "DIT Regional Website",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "business_activity" : {
+      "name" : "Retail",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "investor_company" : {
+      "name" : "Company Registered Name Ltd",
+      "id" : "046b6c7f-0b8a-43b9-b35d-6489e6daee91"
+    },
+    "project_code" : "P-01234567",
+    "referral_source_activity" : {
+      "name" : "Cold Call",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "project_shareable" : true,
+    "referral_source_advisor" : "",
+    "nda_signed" : false,
+    "investment_type" : {
+      "name" : "FDI",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "investment_recipient_company" : "",
+    "non_fdi_type" : {
+      "name" : "Advanced Engineering Supply Chain",
+      "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
+    },
+    "name" : "Marriot Hotels - New hotel in Manchester",
+    "intermediate_company" : ""
+  } ]
+};
+  if (Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  } else {
+    res.end();
+  }
+}
+
 exports.metadataBusinessActibvity = function(args, res, next) {
   /**
    * retrieves Business Activity metadata array
